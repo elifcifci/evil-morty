dff171555f39c6067eea726178dc4b6353ba99d5
Elif Çiftçi <eliffbat@gmail.com>
2024-03-01 18:46:01 +0300

    feat: #OeJTemU5 serched character querys in the list gets from api

--- src/components/organisms/Search/index.tsx
+++ src/components/organisms/Search/index.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import SeachItem from "../../molecules/SearchItem/index";
 import SeachInput from "../../molecules/SearchInput/index";
 import { getRicks } from "../../../api/ricks-api";
-import {IAllCharacterProp} from "../../../interfaces/apiInterfaces"
+import { IAllCharacterProp } from "../../../interfaces/apiInterfaces";
+import {quotesFromMorty} from "../../../constants/quotesFromMorty";
 import styles from "./search.module.scss";
 
 const Seach = () => {
   const [value, setValue] = React.useState("");
   const [apiData, setApiData] = React.useState([]);
   const [selectedCharacters, setSelectedCharacters] = React.useState([]);
+  const [isEvilMode, setIsEvilMode] = React.useState(false);
 
   React.useEffect(() => {
     getRicks()
@@ -18,19 +20,32 @@ const Seach = () => {
       .catch((err) => console.log(err));
   }, []);
 
+  const filteredList = apiData.filter((obj: IAllCharacterProp) =>
+    obj.name.toLowerCase().includes(value.toLowerCase())
+  );
+
   return (
     <div className={styles.seachWrapper}>
       <SeachInput value={value} setValue={setValue} />
       <ul className={styles.searchItems}>
-        {apiData.map((obj: IAllCharacterProp) => (
-          <SeachItem
-            key={`${obj.id}-${obj.name}`}
-            id={obj.id.toString()}
-            name={obj.name}
-            image={obj.image}
-            episodes={obj.episode.length}
-          />
-        ))}
+        {filteredList.length === 0 ? (
+          <li className={styles.quote}>
+            {isEvilMode ? quotesFromMorty[0] : "Please, listen to Evil morty!"}
+          </li>
+        ) : (
+          filteredList.map((obj: IAllCharacterProp) => {
+            return (
+              <SeachItem
+                key={`${obj.id}-${obj.name}`}
+                inputValue={value}
+                id={obj.id.toString()}
+                name={obj.name.trim()}
+                image={obj.image}
+                episodes={obj.episode.length}
+              />
+            );
+          })
+        )}
       </ul>
     </div>
   );
