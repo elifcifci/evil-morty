e112808d4a8e2bef88b4eb76aedddf0bd4a2626e
Elif Çiftçi <eliffbat@gmail.com>
2024-03-19 15:58:03 +0300

    fix: #mwH3IFgt header will not seem on landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,30 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import MortyUniverse from "./pages/MortyUniverse";
 import Header from "./components/organisms/Header";
 
 function App() {
-  const pathname = window.location.pathname;
   const [isEvilMode, setIsEvilMode] = React.useState(false);
+  const location = useLocation();
 
   return (
     <>
-      <BrowserRouter>
-        {pathname !== "/" && <Header isEvilMode={isEvilMode} />}
-        <Routes>
-          <>
-            <Route path="/" element={<LandingPage />} />
-            <Route
-              path="/evil-morty"
-              element={
-                <MortyUniverse
-                  isEvilMode={isEvilMode}
-                  setIsEvilMode={setIsEvilMode}
-                />
-              }
-            />
-          </>
-        </Routes>
-      </BrowserRouter>
+      {location.pathname !== "/" && <Header isEvilMode={isEvilMode} />}
+      <Routes>
+        <>
+          <Route path="/" element={<LandingPage />} />
+          <Route
+            path="/evil-morty"
+            element={
+              <MortyUniverse
+                isEvilMode={isEvilMode}
+                setIsEvilMode={setIsEvilMode}
+              />
+            }
+          />
+        </>
+      </Routes>
     </>
   );
 }
